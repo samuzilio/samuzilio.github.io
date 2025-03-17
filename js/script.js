4e19d46505783e90b259fbec9c6201104747fc54
Samuele Zilio <94171193+samuzilio@users.noreply.github.com>
2025-03-17 08:55:00 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,1 @@
 // js/script.js
-
-// function getRandomColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
-
-// function changeBackgroundColor(event) {
-//   if (event.target.tagName.toLowerCase() === 'a' || event.target.closest('.emoji')) {
-//     return;
-//   }
-
-//   const randomColor = getRandomColor();
-//   document.body.style.backgroundColor = randomColor;
-// }
-
-// document.body.addEventListener('click', changeBackgroundColor);
