c949548af9ad8a3fca1e4029dad1a0d4fa367e6f
Samuele Zilio <94171193+samuzilio@users.noreply.github.com>
2026-04-22 16:28:33 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,51 +1,58 @@
 // js/script.js
 
 const shooterElement = document.getElementById("shooter");
-shooterElement.width = 800;
-shooterElement.height = 600;
 const ctx = shooterElement.getContext("2d");
-
 const targetElement = document.getElementById("target");
-const textY = targetElement.getBoundingClientRect().top;
-
 let highlighttarget = false;
+
+function resizeCanvas() {
+    shooterElement.width = window.innerWidth;
+    shooterElement.height = window.innerHeight;
+}
+resizeCanvas();
+
+function getTextY() {
+    return targetElement.getBoundingClientRect().top;
+}
 
 class Player {
     constructor() {
-        this.x = shooterElement.width / 2 - 10;
-        this.y = shooterElement.height - 40;
-        this.width = 20;
-        this.height = 20;
-        this.color = "#FFFFFF";
-        this.speed = 3;
-        this.targetX = this.x + 80;
+        this.reset();
         this.bullets = [];
         this.shooting = false;
         this.startMovement = false;
     }
 
+    reset() {
+        this.width = 20;
+        this.height = 20;
+        this.color = "#FFFFFF";
+        this.speed = 3;
+        this.x = shooterElement.width / 2 - 10;
+        this.y = shooterElement.height - 40;
+        this.targetX = this.x + 80;
+    }
+
     draw() {
         ctx.fillStyle = this.color;
-    
         ctx.fillRect(this.x + 6, this.y, 8, 4);
         ctx.fillRect(this.x + 4, this.y + 4, 12, 4);
         ctx.fillRect(this.x, this.y + 8, 20, 4);
-    
         ctx.fillRect(this.x, this.y + 4, 4, 4);
         ctx.fillRect(this.x + 16, this.y + 4, 4, 4);
-    
+
         const isMobile = window.innerWidth <= 768;
-        const stopBeforetarget = textY + (isMobile ? 60 : 40);
-    
+        const stopBeforeTarget = getTextY() + (isMobile ? 60 : 40);
+
         this.bullets.forEach((bullet, index) => {
             bullet.y -= bullet.speed;
-    
-            if (!highlighttarget && bullet.y > stopBeforetarget) {
+
+            if (!highlighttarget && bullet.y <= stopBeforeTarget) {
                 highlighttarget = true;
                 targetElement.classList.add("underline");
             }
-    
-            if (bullet.y > stopBeforetarget) {
+
+            if (bullet.y > stopBeforeTarget) {
                 ctx.fillStyle = bullet.color;
                 ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
             } else {
@@ -53,7 +60,7 @@ class Player {
             }
         });
     }
-    
+
     update() {
         if (this.startMovement) {
             if (this.x < this.targetX) {
@@ -80,20 +87,25 @@ class Player {
     }
 
     startAutoFire() {
-        setInterval(() => {
-            this.shoot();
-        }, 500);
+        setInterval(() => this.shoot(), 500);
     }
 
     startMovementAfterDelay() {
-        setTimeout(() => {
-            this.startMovement = true;
-        }, 5000);
+        setTimeout(() => { this.startMovement = true; }, 5000);
     }
 }
 
 const player = new Player();
 player.startMovementAfterDelay();
+
+// On resize: resize canvas and reposition the player
+window.addEventListener("resize", () => {
+    resizeCanvas();
+    player.reset();
+    player.bullets = [];
+    highlighttarget = false;
+    targetElement.classList.remove("underline");
+});
 
 function update() {
     ctx.clearRect(0, 0, shooterElement.width, shooterElement.height);
@@ -101,5 +113,4 @@ function update() {
     player.draw();
     requestAnimationFrame(update);
 }
-
 update();
